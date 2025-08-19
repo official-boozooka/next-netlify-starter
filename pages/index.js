04d86e7a107b790081bcf38cf02f2bab596cd9e5
official-boozooka <boozooka.com@gmail.com>
2025-08-19 17:12:15 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Stay tuned for BOOZOOKA updates!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          follow us on all platforms @official.boozooka
         </p>
       </main>
 
